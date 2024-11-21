e7886e21df77a378e15344a44f28376bf6e23535
sneaky-es <s66342935@gmail.com>
2024-11-21 14:41:23 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * This is a sample file that will have a few violations for code analysis
+ * Violations:
+ *  - File is not in a named package.
+ *  - Empty method
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        emptyMethod();
+    }
+
+    public static void emptyMethod() {
+
+    }
+}
